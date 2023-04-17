0669a3229ff236d9f1d7052a3935cfd4efd2d919
Ovchinnikov Andrew <63587191+AndrewOvvv@users.noreply.github.com>
2023-04-17 17:05:24 +0300

    Update board.hpp

--- include/board.hpp
+++ include/board.hpp
@@ -15,8 +15,15 @@ class Board {
 private:
     int64_t hp_{};
     
-    element::Element state_; 
+    element::Element* state_;
+    
+    std::vector<effect::Effect*> current_effects_;
 public:
+    auto hp() const {
+        return hp_;
+    } // return Board::hp_
+    
+    void apply_effects() {} // no return value; just apply effects
 }; // class Board
 
 } // namespace Game

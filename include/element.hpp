6fb78d78eb0427b2fe652a798d5f22a2873ca029
Ovchinnikov Andrew <63587191+AndrewOvvv@users.noreply.github.com>
2023-04-17 16:43:54 +0300

    Update element.hpp

--- include/element.hpp
+++ include/element.hpp
@@ -12,7 +12,7 @@ class Element
 {
 private:
     std::string name_{};
-    std::string decription_();
+    std::string description_();
     // name - NAME of element (for output, debugging, domestic using and for user)
     // decription - string describes Element (especially for players)
 public:
@@ -20,8 +20,8 @@ public:
         return name_;
     } // return Element::name_ 
     
-    std::string decription() const {
-        return decription_;
+    std::string description() const {
+        return description_;
     } // return Element::decription_
     
     virtual auto affect() const = 0;

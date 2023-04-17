223519959f6aa44224c32cc696d7df60d8c0ad9e
Ovchinnikov Andrew <63587191+AndrewOvvv@users.noreply.github.com>
2023-04-17 17:15:21 +0300

    Update board.hpp

--- include/board.hpp
+++ include/board.hpp
@@ -13,15 +13,29 @@ namespace Game
 
 class Board {
 private:
+    using element_t = std::unique_ptr<element::Element>;
+    using effect_t = std::unique_ptr<effect::Effect>;
+    
     int64_t hp_{};
     
-    element::Element* state_;
+    element_t state_;
     
-    std::vector<effect::Effect*> current_effects_;
+    std::vector<effect_t> current_effects_;
 public:
+    
+    // GETTERS
     auto hp() const {
         return hp_;
     } // return Board::hp_
+    
+    auto state() const {
+        return state_;
+    } // return Board::state_
+    
+    // SETTERS
+    auto state(element_t new_element) {
+        state_ = new_element;
+    } // no return value; set new Board::state_ value
     
     // TODO
     void apply_effects() {} // no return value; just apply effects
